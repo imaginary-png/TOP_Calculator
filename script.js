7940ff5d1f85697cd9745ccfd469d4c542727487
imaginary-png <will_sun@y7mail.com>
2022-02-02 18:35:11 +1100

    added default '0' in an empty input display

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+const DEFAULT_FONT_SIZE = '50px';
+const DEFAULT_MAX_INPUT_LENGTH = 11;
+
+const inputDisplay = document.getElementById('calc-input');
 
 const calcNumbers = document.getElementsByClassName('calc-number');
 const calcOperators = document.getElementsByClassName('calc-operator');
 
-const divide = document.getElementById('divide');
-const multiply = document.getElementById('multiply');
-const add = document.getElementById('add');
-const subtract = document.getElementById('subtract');
+const divideOperator = document.getElementById('divide');
+const multiplyOperator = document.getElementById('multiply');
+const addOperator = document.getElementById('add');
+const subtractOperator = document.getElementById('subtract');
 
-const clear = document.getElementById('clear');
-const del = document.getElementById('del');
+const clearOperator = document.getElementById('clear');
+const delOperator = document.getElementById('del');
 
-const decimal = document.getElementById('decimal');
-const equals = document.getElementById('equals');
+const decimalOperator = document.getElementById('decimal');
+const equalsOperator = document.getElementById('equals');
 
+let inputMaxLength = DEFAULT_MAX_INPUT_LENGTH; //default 11, input length for resizing
+let firstNum;
+let secondNum;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -31,6 +38,57 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-window.onload(){
-    
+
+//input functions 
+
+function updateInput() {
+    if (inputDisplay.innerText == '0') { inputDisplay.innerText = '' } //remove starting 0
+
+    //only allow 22 numbers, otherwise not enough room and why would you use so many anyway? this is a basic calc.
+    if (inputDisplay.innerText.length == 22) { return }
+    inputDisplay.innerText += this.innerText;
+
+    //lower fontsize if the input box is filling up.
+    let currentFontSize = parseInt(window.getComputedStyle(inputDisplay).fontSize.split('px'));
+
+    if (inputDisplay.innerText.length > inputMaxLength && currentFontSize > 26) {
+        inputDisplay.style.fontSize = `${currentFontSize - 8}px`;
+        inputMaxLength += 3;
+    }
+}
+
+function clearInput() {
+    inputDisplay.innerText = '0';
+    inputDisplay.style.fontSize = DEFAULT_FONT_SIZE;
+    inputMaxLength = DEFAULT_MAX_INPUT_LENGTH;
+}
+
+function deleteFromInput() {
+    inputArray = Array.from(inputDisplay.innerText);
+
+    if (inputArray.pop()) {
+        inputDisplay.innerText = inputArray.join('');
+    }
+
+    if (inputArray.length == 0) {
+        inputDisplay.innerText = '0';
+        return;
+    }
+
+    //increase fontsize if needed.
+    let currentFontSize = parseInt(window.getComputedStyle(inputDisplay).fontSize.split('px'));
+
+    let defaultFontSize = parseInt(DEFAULT_FONT_SIZE.split('px'));
+    if (inputDisplay.innerText.length < inputMaxLength - 2 && currentFontSize < defaultFontSize) {
+        inputDisplay.style.fontSize = `${currentFontSize + 8}px`;
+        inputMaxLength -= 3;
+    }
+}
+
+window.onload = () => {
+    Array.from(calcNumbers).forEach((number) =>
+        number.addEventListener('click', updateInput));
+
+    clearOperator.addEventListener('click', clearInput);
+    delOperator.addEventListener('click', deleteFromInput);
 }
