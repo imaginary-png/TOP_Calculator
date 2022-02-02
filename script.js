02f206046d4d36204129ee87380088f675c35e5e
imaginary-png <will_sun@y7mail.com>
2022-02-02 17:01:46 +1100

    added basic functions, worked on css layout design

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const calcNumbers = document.getElementsByClassName('calc-number');
+const calcOperators = document.getElementsByClassName('calc-operator');
+
+const divide = document.getElementById('divide');
+const multiply = document.getElementById('multiply');
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+
+const clear = document.getElementById('clear');
+const del = document.getElementById('del');
+
+const decimal = document.getElementById('decimal');
+const equals = document.getElementById('equals');
+
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+window.onload(){
+    
+}
